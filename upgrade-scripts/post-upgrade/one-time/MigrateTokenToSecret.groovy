dabafe1da5b94475349b68d507c8c5abf6b3ad90
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2022-10-24 16:41:19 +0000

    [EDP-DDM-20125] Add upgrade script to migrate edr-registry authorization-token to secret
    Change-Id: I684386e81c91cd1a7025444a22e262dab0ae2777

---
+++ upgrade-scripts/post-upgrade/one-time/MigrateTokenToSecret.groovy
@@ -0,0 +1,32 @@
+void call() {
+    String secretName = "trembita-registries-secrets"
+    String gerritSecretName = "gerrit-ciuser-password"
+    String registryRepoName = "registry-regulations"
+    String gerritUser = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.user} " +
+            "-n $NAMESPACE | base64 --decode", returnStdout: true)
+    String gerritPass = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.password} " +
+            "-n $NAMESPACE | base64 --decode", returnStdout: true)
+    String gerritHost = sh(script: "oc get route gerrit -o jsonpath={.spec.host} -n $NAMESPACE", returnStdout: true)
+    String repoUrl = "https://$gerritUser:$gerritPass@$gerritHost/$registryRepoName"
+    sh(script: "set +x; git clone $repoUrl")
+    def configFile = readYaml file: "$registryRepoName/bp-trembita/configuration.yml"
+    def edrRegistry = configFile["trembita-exchange-gateway"]["registries"]["edr-registry"]
+    if (edrRegistry["authorization-token"] != null) {
+        String token = configFile["trembita-exchange-gateway"]["registries"]["edr-registry"]["authorization-token"].bytes.encodeBase64().toString()
+        sh(script: "set +x; oc patch secret $secretName --type merge -p '{\"data\": {\"trembita.registries.edr-registry.auth.secret.token\": \"$token\"}}' -n $NAMESPACE")
+        edrRegistry.put("secret-name", secretName)
+        edrRegistry.remove("authorization-token")
+        sh(script: "rm $registryRepoName/bp-trembita/configuration.yml")
+        writeYaml file: "$registryRepoName/bp-trembita/configuration.yml", data: configFile
+        sh(script: "set +x; cd $registryRepoName " +
+                "&& git config user.name \"$gerritUser\" " +
+                "&& git config user.email \"jenkins@example.com\" " +
+                "&& git config user.password \"$gerritPass\" " +
+                "&& git add . && git commit -m 'Move token to secret' " +
+                "&& git push origin master")
+        sh(script: "oc scale deployment/bpms --replicas=0 -n $NAMESPACE || :")
+        sh(script: "oc scale deployment/bpms --replicas=1 -n $NAMESPACE || :")
+    }
+}
+
+return this;
