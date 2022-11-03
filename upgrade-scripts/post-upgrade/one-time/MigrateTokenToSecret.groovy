6b94d0807436db31552de9d1617e66cc59a03ac2
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2022-11-03 14:49:54 +0000

    [EDP-DDM-20125] Fixed MigrateTokenToSecret upgrade script
    Change-Id: Ie7f6d631f927e68c8dd32451ae43eaa64357a4ac

--- upgrade-scripts/post-upgrade/one-time/MigrateTokenToSecret.groovy
+++ upgrade-scripts/post-upgrade/one-time/MigrateTokenToSecret.groovy
@@ -1,19 +1,18 @@
 void call() {
-    String revision = sh(script: "helm status registry-configuration -n $NAMESPACE | grep REVISION | sed 's/^.*://'", returnStdout: true).trim()
-    if (revision.toInteger() > 1) {
-        String secretName = "trembita-registries-secrets"
-        String gerritSecretName = "gerrit-ciuser-password"
-        String registryRepoName = "registry-regulations"
-        String gerritUser = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.user} " +
-                "-n $NAMESPACE | base64 --decode", returnStdout: true)
-        String gerritPass = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.password} " +
-                "-n $NAMESPACE | base64 --decode", returnStdout: true)
-        String gerritHost = sh(script: "oc get route gerrit -o jsonpath={.spec.host} -n $NAMESPACE", returnStdout: true)
-        String repoUrl = "https://$gerritUser:$gerritPass@$gerritHost/$registryRepoName"
-        String isRepoExists = sh(script: "set +x; git ls-remote $repoUrl > /dev/null", returnStatus: true)
-        if (isRepoExists == '0') {
-            sh(script: "set +x; git clone $repoUrl")
-            def configFile = readYaml file: "$registryRepoName/bp-trembita/configuration.yml"
+    String secretName = "trembita-registries-secrets"
+    String gerritSecretName = "gerrit-ciuser-password"
+    String registryRepoName = "registry-regulations"
+    String gerritUser = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.user} " +
+            "-n $NAMESPACE | base64 --decode", returnStdout: true)
+    String gerritPass = sh(script: "oc get secret $gerritSecretName -o jsonpath={.data.password} " +
+            "-n $NAMESPACE | base64 --decode", returnStdout: true)
+    String gerritHost = "gerrit.${NAMESPACE}.svc.cluster.local:8080"
+    String repoUrl = "http://$gerritUser:$gerritPass@$gerritHost/$registryRepoName"
+    String isRepoExists = sh(script: "set +x; git ls-remote $repoUrl > /dev/null", returnStatus: true)
+    if (isRepoExists == '0') {
+        sh(script: "set +x; git clone $repoUrl")
+        def configFile = readYaml file: "$registryRepoName/bp-trembita/configuration.yml"
+        if (configFile["trembita-exchange-gateway"]["registries"].containsKey("edr-registry")) {
             def edrRegistry = configFile["trembita-exchange-gateway"]["registries"]["edr-registry"]
             if (edrRegistry["authorization-token"] != null) {
                 String token = configFile["trembita-exchange-gateway"]["registries"]["edr-registry"]["authorization-token"].bytes.encodeBase64().toString()
