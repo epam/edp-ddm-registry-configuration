37507b68ef6ea47d2a6aceace7012703fba3206f
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2023-02-02 19:04:27 +0000

    [EDP-DDM-19050] Add returnStdout
    Change-Id: I1d3519356918f10c8a364a19806c56925e6d84cd

--- upgrade-scripts/post-install/common/SetToken.groovy
+++ upgrade-scripts/post-install/common/SetToken.groovy
@@ -3,7 +3,7 @@ void call() {
     String newToken = sh(script: "set +x; oc -n $NAMESPACE get secret trembita-registries-secrets -o jsonpath={.data.\"trembita\\.registries\\.edr\\-registry\\.auth\\.secret\\.token\"} " +
             "-n $NAMESPACE | base64 --decode || :", returnStdout: true)
     sh(script: "set +x; oc -n $NAMESPACE get keycloakauthflow citizen-portal-dso-citizen-auth-flow -o json | jq '.spec.authenticationExecutions[1].authenticatorConfig.config.registryToken = \"${newToken}\"' | oc replace -n $NAMESPACE --force -f - || :")
-    String podCount = sh(script: "oc -n $NAMESPACE get pods -l name=keycloak-operator --no-headers | wc -l").trim()
+    String podCount = sh(script: "oc -n $NAMESPACE get pods -l name=keycloak-operator --no-headers | wc -l", returnStdout: true).trim()
     sh "oc scale deployment/keycloak-operator --replicas=0 -n $NAMESPACE || :"
     sh "oc scale deployment/keycloak-operator --replicas=$podCount -n $NAMESPACE || :"
 }
