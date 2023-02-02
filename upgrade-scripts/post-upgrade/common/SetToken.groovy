c0d35a1e909226deaace0f2eec59283e198a053a
Oleh Boreiko1 <oleh_boreiko1@epam.com>
2023-02-02 18:46:47 +0000

    [EDP-DDM-19050] Hot fix
    Change-Id: I09c631941a063de7870260850aac59c48ee8f365

--- upgrade-scripts/post-upgrade/common/SetToken.groovy
+++ upgrade-scripts/post-upgrade/common/SetToken.groovy
@@ -3,7 +3,9 @@ void call() {
     String newToken = sh(script: "set +x; oc -n $NAMESPACE get secret trembita-registries-secrets -o jsonpath={.data.\"trembita\\.registries\\.edr\\-registry\\.auth\\.secret\\.token\"} " +
             "-n $NAMESPACE | base64 --decode || :", returnStdout: true)
     sh(script: "set +x; oc -n $NAMESPACE get keycloakauthflow citizen-portal-dso-citizen-auth-flow -o json | jq '.spec.authenticationExecutions[1].authenticatorConfig.config.registryToken = \"${newToken}\"' | oc replace -n $NAMESPACE --force -f - || :")
-    sh "oc rollout restart deployment/keycloak-operator -n $NAMESPACE || :"
+    String podCount = sh(script: "oc -n $NAMESPACE get pods -l name=keycloak-operator --no-headers | wc -l").trim()
+    sh "oc scale deployment/keycloak-operator --replicas=0 -n $NAMESPACE || :"
+    sh "oc scale deployment/keycloak-operator --replicas=$podCount -n $NAMESPACE || :"
 }
 
 return this;
